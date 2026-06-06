82f6368a181110641bd3ea17f4183240afdc895c
andressoirifman <andres.soirifman@gmail.com>
2026-06-06 11:54:33 -0300

    Add components to the main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,23 @@
+import Header from './Header';
+import Hero from './Hero';
+import Dolor from './Dolor';
+import Modulos from './Modulos';
+import Entregables from './Entregables';
+import Testimonios from './Testimonios';
+import CTAFinal from './CTAFinal';
+import Footer from './Footer';
 
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <Header />
+      <Hero />
+      <Dolor />
+      <Modulos />
+      <Entregables />
+      <Testimonios />
+      <CTAFinal />
+      <Footer />
+    </main>
+  );
+}
