ec0ab1517b80be8345c0b54c0bf1653004ad1afe
andressoirifman <andres.soirifman@gmail.com>
2026-06-19 21:10:34 -0300

    Change image opacity from 0 to 1

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
           style={{
             objectFit:    "cover",
             objectPosition: "center",
-            opacity:      0,
+            opacity:     1,
             filter:       "grayscale(75%) contrast(1.2)",
           }}
         />
